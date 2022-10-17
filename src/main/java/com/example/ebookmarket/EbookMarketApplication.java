bddaf4348eb915744a0f4c23ea6a894998f31979
ap3334 <chwil2989@naver.com>
2022-10-17 11:15:17 +0900

    feat: Init setting

---
+++ src/main/java/com/example/ebookmarket/EbookMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.ebookmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbookMarketApplication.class, args);
+    }
+
+}
